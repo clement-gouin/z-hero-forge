2c8a406e7c66e640ee45dadef74f064aad955df9
Clément Gouin <clement.gouin@nameshield.net>
2025-05-23 10:05:51 +0200

    update arguments

--- README.txt
+++ README.txt
@@ -1,11 +1,19 @@
-usage: forge.py [-h] [-p] [--dry] DIR
+usage: forge.py [-h] [-n NAMESPACE] [-c COLOR] [-s START] [-p] [-d] DIR
 
 creates a z-hero-quest adventure from markdown data (see sample directory)
 
 positional arguments:
-  DIR            data file path (default: data.txt)
+  DIR                   data file path (default: data.txt)
 
 options:
-  -h, --help     show this help message and exit
-  -p, --preview  show links tree in a preview.png file
-  --dry          do not compute links
+  -h, --help            show this help message and exit
+  -n NAMESPACE, --namespace NAMESPACE
+                        hero quest namespace (default: random)
+  -c COLOR, --color COLOR
+                        hero quest color as: Hue (number), Saturation
+                        (percent) (default: 180, 30%)
+  -s START, --start START
+                        start scene (default: "start.md" or first file by
+                        name)
+  -p, --preview         show links tree in a preview.png file
+  -d, --dry             do not compute links
--- forge.py
+++ forge.py
@@ -17,11 +17,7 @@ def __main():
         description="creates a z-hero-quest adventure from markdown data (see sample directory)"
     )
     parser.add_argument(
-        "-p",
-        "--preview",
-        action="store_true",
-        help="show links tree in a preview.png file",
-        default=False,
+        "dir_path", help="data file path (default: data.txt)", metavar="DIR"
     )
     parser.add_argument(
         "-n",
@@ -31,13 +27,32 @@ def __main():
         default=None,
     )
     parser.add_argument(
+        "-c",
+        "--color",
+        help="hero quest color as: Hue (number), Saturation (percent)  (default: 180, 30%%)",
+        required=False,
+        default=None,
+    )
+    parser.add_argument(
+        "-s",
+        "--start",
+        help='start scene (default: "start.md" or first file by name)',
+        required=False,
+        default=None,
+    )
+    parser.add_argument(
+        "-p",
+        "--preview",
+        action="store_true",
+        help="show links tree in a preview.png file",
+        default=False,
+    )
+    parser.add_argument(
+        "-d",
         "--dry",
         action="store_true",
         help="do not compute links",
         default=False,
-    )
-    parser.add_argument(
-        "dir_path", help="data file path (default: data.txt)", metavar="DIR"
     )
     args = parser.parse_args()
     # TODO
